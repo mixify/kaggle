6dee994bae82e4a4e083d695d857a0e02901da0d
mixify <mixify@github.com>
2019-04-08 23:35:27 +0900

    tutrial solutions

--- titanic/kernel.py
+++ titanic/kernel.py
@@ -2,3 +2,16 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import missingno as msno
+import warnings
+
+warnings.filterwarnings('ignore')
+
+plt.style.use('seaborn')
+
+sns.set(font_scale=2.5)
+
+from subprocess import check_output
+
+# % matplotlib inline
+
